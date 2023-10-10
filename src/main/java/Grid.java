ce455dd8a9dc64df4aa89c826c453ae6e2d9c31b
viviane <vivianebinet@hotmail.com>
2023-10-10 14:15:30 -0400

    Grid class

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -3,34 +3,72 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
+
 public class Grid {
     private int height;
     private int width;
-    private char[] grid;
+    //private char[][] grid;
     private ArrayList<String> words;
 
 
     //      Getters & Setters
     public int getHeight(){return this.height;}
-
     public int getWidth() {return width;}
-
-    public char[] getGrid() {return grid;}
+   // public char[][] getGrid() {return grid;}
     public ArrayList<String> getWords() {return words;}
     public void setHeight(int height){this.height = height;}
     public void setWidth(int width){this.width = width;}
-    public void setGrid(char[] grid) {this.grid = grid;}
+    //public void setGrid(char[][] grid) {this.grid = grid;}
     public void setWords(ArrayList<String> words) {this.words = words;}
 
     // File reader
-    public void fileReader(String filePath) throws FileNotFoundException {
+    public void readFile(String filePath) throws FileNotFoundException {
         File textFile = new File(filePath);
         Scanner reader = new Scanner(textFile);
-        while (reader.hasNextLine()) {
-            System.out.println(reader.nextLine());
+
+      //  while (reader.hasNextLine()) {
+      //      String line = reader.nextLine();
+      //      String[] parts = line.split(" ");
+      //      setHeight(parseInt(parts[0]));
+      //      setWidth(parseInt(parts[1]));
+      //      System.out.println(parts);
+      //  }
+
+        // Height and width
+        String line = reader.nextLine();
+        String[] numbers = line.split(" ");
+        setHeight(parseInt(numbers[0]));
+        setWidth(parseInt(numbers[1]));
+       // ArrayList<Character> grid = new ArrayList<>();
+         String[][] grid = new String[getHeight()][getWidth()];
+
+        // Letters
+        for (int i = 0; i < this.getHeight(); i++) {
+            line = reader.nextLine();
+            String[] letters = line.split(" ");
+            grid[i] = letters;
+
+            }
+
+        System.out.println(grid[0][2]);
+
+
+        // Words
+        line = reader.nextLine();
+        String[] words = line.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]);
         }
+        // Put in a ArrayList or keep in a table?
 
     }
 
-
 }
+
+/* class Words
+* class Grid: height, width, letters
+*
+ */
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Grid myGrid = new Grid();
 
-        //test
-        myGrid.setHeight(3);
-        System.out.println(myGrid.getHeight());
 
         try {
-            myGrid.fileReader("C:\\_Code\\courses\\IFT2015_TP1\\src\\main\\resources\\TP1Input");
+            myGrid.readFile("C:\\_Code\\courses\\IFT2015_TP1\\src\\main\\resources\\TP1Input");
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        System.out.println(myGrid.getHeight());
     }
 }
