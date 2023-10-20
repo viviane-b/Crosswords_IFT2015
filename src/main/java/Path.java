41e450914faa5a8adfebe3d7b60807139b05034b
viviane <vivianebinet@hotmail.com>
2023-10-20 14:32:18 -0400

    print words paths

--- src/main/java/Path.java
+++ src/main/java/Path.java
@@ -1,9 +1,81 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
 
 public class Path extends ArrayList {
     private String word;
-    private ArrayList<Object> list;
+    private ArrayList<String> list = new ArrayList<>();         // should be a linkedlist so it's faster to remove(index=0)
+  //    private Queue<String> coordinates = new Queue<String>();
+
+    public Path (String word) {
+        this.word = word;
+       // this.list = list;
+       // this.coordinates = coordinates;
+    }
+
+ // public Queue<String> getCoordinates() {
+ //     return coordinates;
+ // }
 
 
+    public String getWord() {
+        return word;
+    }
+
+    public ArrayList<String> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<String> list) {
+        this.list = list;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+ //  public void setCoordinates(Queue<String> coordinates) {
+ //      this.coordinates = coordinates;
+ //  }
+
+    public void add (String element) {
+        this.list.add(element);
+    }
+
+    public void removeLast () {
+        this.list.remove(this.list.size()-1);
+    }
+    public void enqueue(String element) {
+        this.list.add(element);
+    }
+    public String dequeue() {
+        return (this.list.remove(0));
+    }
+    public void adjustPath(int index) {
+        for (int i = index; i < this.list.size(); i++) {
+            this.list.remove(i);
+        }
+    }
+
+    public void printPath(){
+        System.out.print(this.word + " ");
+        for (int i=0; i< this.list.size()-1; i++) {
+            System.out.print(this.list.get(i) + "->");
+        }
+        System.out.println(this.list.get(this.list.size()-1));
+
+    }
+
+    public void printPath1() {
+        int listSize = this.list.size();
+        System.out.println("size of queue before printPath: " + this.list.size());
+        System.out.print(this.word + " ");
+        for (int i=0; i < listSize -1; i++) {
+            System.out.print(this.list.remove(0) + "->");
+        }
+        System.out.println(this.list.remove(0));
+        System.out.println("size of queue after printPath: " + this.list.size());
+
+    }
 
 }
--- src/main/java/PatternSearch.java
+++ src/main/java/PatternSearch.java
@@ -4,9 +4,10 @@ public class PatternSearch {
     private int[] y = {-1, 0, 1, -1, 1, -1, 0, 1 };
     private char[][] grid;
     private char[][] words;
-    private ArrayList<String> path = new ArrayList<>();
+    //private ArrayList<String> path = new ArrayList<>();
 
     //private String word;
+
 
     public PatternSearch(char[][] grid, char[][] words) {
         this.grid = grid;
@@ -16,8 +17,37 @@ public class PatternSearch {
 
     // TODO: do another function isAround() that is not recursive :(
 
-    public boolean isAround (int x0, int y0, char[] word){
-        this.path.add("(" + x0 + "," + y0 + ")");
+
+    public boolean isAround2 (int x0, int y0, char[] word) {
+        for (int move = 0; move<8; move++) {
+            int i;
+            int row = x0 + x[move];
+            int col = y0 + y[move];
+
+            for (i = 1; i < word.length; i++) {
+                if (row < 0 || row >= this.grid.length || col < 0 || col >= this.grid[0].length) {
+                    break;
+                }
+                if (this.grid[row][col] != word[i]) {
+                    break;
+                }
+                System.out.println("letter " + word[i] + " was found at position (" + row + "," + col + ").");
+                //row += x[move];
+                //col += y[move];
+            }
+            if (i == word.length) {
+                System.out.println("SUCCESS for word " + new String(word) + "\n");
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    public boolean isAround (int x0, int y0, char[] word, Path path){
+        //path.setList(path.getList().add("(" + x0 + "," + y0 + ")"));
+        path.add("(" + x0 + "," + y0 + ")");
 
         if (word.length == 1) {
         //    System.out.println("**************");
@@ -47,51 +77,111 @@ public class PatternSearch {
                     //this.path.add("(" + row + "," + col + ")");
    //                 System.out.println("letter " + word[1] + " was found at position (" + row + "," + col + ").");
     //                System.out.println("array is of length " + word.length);
-                    if (isAround(row, col, Arrays.copyOfRange(word, 1, word.length))) {
-    //                    System.out.println("TRUE");
-    //                    return (true);
-                        printPath(new String(word), this.path);
-                        this.path.clear();
-                        System.out.println("\n");
+                 //   if (isAround(row, col, Arrays.copyOfRange(word, 1, word.length))) {
+    //           //         System.out.println("TRUE");
+    //           //         return (true);
+                 //       printPath(new String(word), this.path);
+                 //       this.path.clear();
+                 //       System.out.println("\n");
+                 //   }
+                    if (word.length > 2) {
+                      isAround(row, col, Arrays.copyOfRange(word, 1, word.length), path);
+                    } else {
+                      System.out.println("**************");
+                      path.printPath();
+                 //     this.path.clear();
+                      return (true);        // the whole word was found!
+                      // recursive function calls the whole stack of precedent isAround so it doesn't return true at the end.
+
                     }
-
-
-                    // if (word.length > 2) {
-                  //     isAround(row, col, Arrays.copyOfRange(word, 1, word.length));
-                  // } else {
-                  //     System.out.println("**************");
-                  //     return (true);        // the whole word was found!
-                  //     // recursive function calls the whole stack of precedent isAround so it doesn't return true at the end.
-
-                  // }
 
                 }
             }
         }
     //    System.out.println("False!");
-        if (this.path.size()>=1) {
-            this.path.remove(this.path.size() - 1);
+        if (path.getList().size()>=1) {
+            path.removeLast();
         }
         return (false);
     }
 
+    public void findWord1(int x0, int y0, char[] word, Path path) {
+
+        if (this.grid[x0][y0] == word[0]) {
+            System.out.println("letter " + word[0] + " was found at position (" + x0 + "," + y0 + ").");
+            path.enqueue("(" + x0 + "," + y0 + ")");
+            if (word.length == 1) {;
+                path.printPath1();
+            } else {
+                for (int move = 0; move<8; move++) {
+                    int row = x0 + x[move];
+                    int col = y0 + y[move];
+                    if (row >= 0 && row < this.grid.length && col >= 0 && col < this.grid[0].length) {
+                        findWord1(row, col, Arrays.copyOfRange(word, 1, word.length), path);
+                    }
+                }
+                if (path.getList().size() != 0) {
+                    path.removeLast();
+                }
+            }
+        }
+
+        // check if the character matches
+        // if true:
+        //      add the coordinates to the queue
+        //      check if it's the last character: printPath
+        //      for all the directions:
+        //          recursive call of the function on the rest of the word
+        // if false:
+        //      pass
+    }
+    public void findWord(int x0, int y0, char[] word, Path path, int length) {
+        int pathLength = length-word.length;
+        path.adjustPath(pathLength);
+        if (this.grid[x0][y0] == word[0]) {
+            System.out.println("letter " + word[0] + " was found at position (" + x0 + "," + y0 + ").");
+            path.enqueue("(" + x0 + "," + y0 + ")");
+            if (word.length == 1) {;
+                path.printPath();
+            } else {
+                for (int move = 0; move<8; move++) {
+                    int row = x0 + x[move];
+                    int col = y0 + y[move];
+                    if (row >= 0 && row < this.grid.length && col >= 0 && col < this.grid[0].length) {
+                        findWord(row, col, Arrays.copyOfRange(word, 1, word.length), path, length);
+
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+    // TODO: put the paths in a tree and then print each leave of the tree with ancestors?
+
+
     public void search() {
         for (int i = 0; i < this.words.length; i++) {
+            Path path = new Path(new String(words[i]));
+
             char first = words[i][0];
             for (int m = 0; m < grid.length; m++) {
                 for (int n = 0; n < grid[m].length; n++) {
                     if (grid[m][n] == first) {  // we have the position of the first letter of the word.
                         System.out.println("letter " + first + " is at position (" + m + "," + n + ").");
-                        if (isAround(m, n, words[i])) {
-        //                    System.out.println("SUCCES! The word " + new String(words[i]) + " was found at position \n");
+                        findWord(m, n, words[i], path, words[i].length);
+        //                if (isAround(m, n, words[i], path)) {
+        //                    System.out.println("SUCCESS! The word " + new String(words[i]) + " was found at position \n");
         //                    System.out.println(this.path);
         //                    printPath(new String(words[i]), this.path);
         //                   this.path.clear();
                             System.out.println("\n");
 
-                        } else {
+        //                } else {
         //                    System.out.println("the word " + new String(words[i]) + " was not found at position (" + + m + "," + n + "). \n");
-                        }
+         //               }
                     }
                 }
             }
